849a70c7fd6c09cab8ffa23ff0c30badbaca0c38
chenyuxiang0425 <chenyuxiang0425@qq.com>
2020-05-28 09:19:06 +0800

    section15 DOM Manipulation

---
+++ section15/selector_exercise.js
@@ -0,0 +1,6 @@
+let a = document.getElementById("first");
+let b = document.getElementsByClassName("special")[0];
+let c = document.getElementsByTagName("p")[0];
+let d = document.querySelector("#first");
+let e = document.querySelectorAll("p")[0];
+let f = document.querySelector("h1 + p");
