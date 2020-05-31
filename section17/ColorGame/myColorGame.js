86f3abb6c416baf45fd8bbf1f38c4d8f0db1422d
chenyuxiang0425 <chenyuxiang0425@qq.com>
2020-05-31 16:10:42 +0800

    section17 finish color game

--- section17/ColorGame/myColorGame.js
+++ section17/ColorGame/myColorGame.js
@@ -1,27 +1,48 @@
-let stripe = document.querySelector("#stripe");
-let message = document.querySelector("#message");
-let squares = document.querySelectorAll(".square");
-let colors = setRandomColors(6);
+let numSquares = 6;
+let colors=[];
 let selectColor;
-
-reset();
+let h1 = document.querySelector("h1");
+let squares = document.querySelectorAll(".square");
+let colorDisplay = document.getElementById('colorDisplay');
+let message = document.querySelector("#message");
+let stripe = document.querySelector("#stripe");
+let resetButton = document.querySelector("#reset");
+let modeButtons = document.querySelectorAll(".mode");
 init();
 
 function init() {
+    setupModeButtons();
     setSquares();
+    reset();
+    resetButton.addEventListener("click",function () {
+        reset();
+    })
+}
+
+function setupModeButtons() {
+    for (let i = 0; i < modeButtons.length; i++) {
+        modeButtons[i].addEventListener("click",function () {
+            for (let i = 0; i < modeButtons.length; i++) {
+                modeButtons[i].classList.remove('selected');
+            }
+            this.classList.add('selected');
+            if (this.textContent === "EASY") numSquares = 3;
+            if (this.textContent === "HARD") numSquares = 6;
+            reset();
+        });
+    }
 }
 
 function setSquares() {
-    let colorDisplay = document.getElementById('colorDisplay');
-    colorDisplay.textContent = selectColor;
-
     for (let i = 0; i < squares.length; i++) {
         // style.background 不能在一些 firefox 上运行
         squares[i].style.backgroundColor = colors[i];
         squares[i].addEventListener('click',function () {
             if (this.style.backgroundColor === selectColor) {
                 message.textContent = "Correct!";
+                resetButton.textContent = "Play again?";
                 changeColors(selectColor);
+                h1.style.backgroundColor = selectColor;
             } else {
                 this.style.backgroundColor = 'rgb(23, 23, 23)';
                 message.textContent = "Try again!";
@@ -31,12 +52,24 @@ function setSquares() {
 }
 
 function reset() {
-    selectColor = pickColor();
+    colors = setRandomColors(numSquares);
+    selectColor = pickColor(numSquares);
+    colorDisplay.textContent = selectColor;
+    resetButton.textContent = "New Colors";
+    message.textContent = "";
+    //change colors of squares
     for (let i = 0; i < squares.length; i++) {
-        // style.background 不能在一些 firefox 上运行
-        squares[i].style.backgroundColor = colors[i];
+        if (colors[i]) {
+            // style.background 不能在一些 firefox 上运行
+            squares[i].style.display = "block";
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
     }
+    h1.style.backgroundColor = "steelblue";
 }
+
 
 function changeColors(color) {
     for (let i = 0; i < squares.length; i++) {
@@ -44,10 +77,10 @@ function changeColors(color) {
     }
 }
 
-function pickColor() {
+function pickColor(num) {
     // Math.floor 去除余数
     // Math.random() 0-1
-    let randomIndex = Math.floor(Math.random() * (6));
+    let randomIndex = Math.floor(Math.random() * (num));
     return colors[randomIndex];
 }
 
@@ -62,3 +95,4 @@ function setRandomColors(num) {
     }
     return colors;
 }
+
