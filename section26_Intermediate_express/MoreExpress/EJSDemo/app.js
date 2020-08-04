1b1548ce9b84bafc7b90e9b17c7ba51a5dc0865c
chenyuxiang0425 <chenyuxiang0425@qq.com>
2020-08-04 21:02:34 +0800

    introduction of ejs

--- section26_Intermediate_express/MoreExpress/EJSDemo/app.js
+++ section26_Intermediate_express/MoreExpress/EJSDemo/app.js
@@ -1,7 +1,22 @@
 const express = require("express");
-
 const app = express();
 
+app.get("/",function (req,res) {
+   res.render("home.ejs")
+});
+
+app.get("/fallinglovewith/:thing",function (req,res) {
+    res.render("love.ejs",{thingVar:req.params.thing})
+})
+
+app.get("/posts",function (req, res) {
+    let posts = [
+        {title:"Post 1", author: "Susy"},
+        {title:"My adorable pet bunny", author: "Charlie"},
+        {title:"Can you believe this pompey", author: "Calt"}
+    ];
+    res.render("posts.ejs",{posts : posts}) 
+})
 
 app.listen(port = 3000,function () {
     console.log("server is starting!")
